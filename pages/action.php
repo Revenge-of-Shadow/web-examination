c4429cf7efd20254cdcef8b451028dcc8a85827b
Revenge-of-Shadow <vyacheslav.pigida@gmail.com>
2023-12-30 00:11:50 +0200

    Topic addition; a lot of sketching.

--- pages/action.php
+++
@@ -1,2 +0,0 @@
-Greetings, <?php echo htmlspecialchars($_POST['name']); ?>.
-You are <?php echo (int)$_POST['age']; ?> years old.
---
+++ pages/sign-out-form.php
@@ -0,0 +1,3 @@
+<form action="sign-out-action.php" method="post">
+    <button type="submit" name="submit" style="background-color: red">Sign out.</button>
+</form>
